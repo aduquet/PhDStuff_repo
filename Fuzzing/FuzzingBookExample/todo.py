8af4ec87c900633f5acf6ef6cff263eaa11a7064
aduquet <duquet@ut.ee>
2022-08-11 16:14:49 +0200

    Fuzzing

---
+++ Fuzzing/FuzzingBookExample/todo.py
@@ -0,0 +1,167 @@
+def cgi_decode(s: str) -> str:
+    """Decode the CGI-encoded string `s`:
+       * replace '+' by ' '
+       * replace "%xx" by the character with hex number xx.
+       Return the decoded string.  Raise `ValueError` for invalid inputs."""
+
+    # Mapping of hex digits to their integer values
+    hex_values = {
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15,
+        'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
+    }
+
+    t = ""
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c == '+':
+            t += ' '
+        elif c == '%':
+            digit_high, digit_low = s[i + 1], s[i + 2]
+            i += 2
+            if digit_high in hex_values and digit_low in hex_values:
+                v = hex_values[digit_high] * 16 + hex_values[digit_low]
+                t += chr(v)
+            else:
+                raise ValueError("Invalid encoding")
+        else:
+            t += c
+        i += 1
+    return t
+
+print(cgi_decode("a+b"))
+
+from ast import List, Set, Tuple
+from types import FrameType, TracebackType
+from typing import Any, Callable, Optional, Type
+import sys
+# from bookutils import print_content, print_file
+import inspect
+
+coverage = []
+
+def traceit(frame: FrameType, event: str, arg: Any) -> Optional[Callable]:
+    """Trace program execution. To be passed to sys.settrace()."""
+    if event == 'line':
+        global coverage
+        function_name = frame.f_code.co_name
+        lineno = frame.f_lineno
+        coverage.append(lineno)
+
+    return traceit
+
+def cgi_decode_traced(s: str) -> None:
+    global coverage
+    coverage = []
+    sys.settrace(traceit)  # Turn on
+    cgi_decode(s)
+    sys.settrace(None)    # Turn off
+    
+cgi_decode_traced("a+b")
+print(coverage)
+cgi_decode_code = inspect.getsource(cgi_decode)
+
+
+covered_lines = set(coverage)
+print(covered_lines)
+
+
+
+# for lineno in range(1, len(cgi_decode_lines)):
+#     if lineno not in covered_lines:
+#         print("# ", end="")
+#     else:
+#         print("  ", end="")
+#     print("%2d  " % lineno, end="")
+#     print_content(cgi_decode_lines[lineno], '.py')
+#     print()
+
+
+# print_content(cgi_decode_code[:300] + "...", ".py")\
+    
+    
+
+Location = Tuple[str, int]
+
+
+
+class Coverage:
+    """Track coverage within a `with` block. Use as
+    ```
+    with Coverage() as cov:
+        function_to_be_traced()
+    c = cov.coverage()
+    ```
+    """
+
+    def __init__(self) -> None:
+        """Constructor"""
+        self._trace: List[Location] = []
+
+    # Trace function
+    def traceit(self, frame: FrameType, event: str, arg: Any) -> Optional[Callable]:
+        """Tracing function. To be overloaded in subclasses."""
+        if self.original_trace_function is not None:
+            self.original_trace_function(frame, event, arg)
+
+        if event == "line":
+            function_name = frame.f_code.co_name
+            lineno = frame.f_lineno
+            if function_name != '__exit__':  # avoid tracing ourselves:
+                self._trace.append((function_name, lineno))
+
+        return self.traceit
+
+    def __enter__(self) -> Any:
+        """Start of `with` block. Turn on tracing."""
+        self.original_trace_function = sys.gettrace()
+        sys.settrace(self.traceit)
+        return self
+
+    def __exit__(self, exc_type: Type, exc_value: BaseException, 
+                 tb: TracebackType) -> Optional[bool]:
+        """End of `with` block. Turn off tracing."""
+        sys.settrace(self.original_trace_function)
+        return None  # default: pass all exceptions
+
+    def trace(self) -> List[Location]:
+        """The list of executed lines, as (function_name, line_number) pairs"""
+        return self._trace
+
+    def coverage(self) -> Set[Location]:
+        """The set of executed lines, as (function_name, line_number) pairs"""
+        return set(self.trace())
+
+    def function_names(self) -> Set[str]:
+        """The set of function names seen"""
+        return set(function_name for (function_name, line_number) in self.coverage())
+
+    def __repr__(self) -> str:
+        """Return a string representation of this object.
+           Show covered (and uncovered) program code"""
+        t = ""
+        for function_name in self.function_names():
+            # Similar code as in the example above
+            try:
+                fun = eval(function_name)
+            except Exception as exc:
+                t += f"Skipping {function_name}: {exc}"
+                continue
+
+            source_lines, start_line_number = inspect.getsourcelines(fun)
+            for lineno in range(start_line_number, start_line_number + len(source_lines)):
+                if (function_name, lineno) in self.trace():
+                    t += "# "
+                else:
+                    t += "  "
+                t += "%2d  " % lineno
+                t += source_lines[lineno - start_line_number]
+
+        return t
+
+with Coverage() as cov:
+    cgi_decode("a+b")
+
+print(cov.coverage())
---
+++ Fuzzing/example1.py
@@ -0,0 +1,27 @@
+
+# def get():
+#     return (3)
+
+# def Prints( a, b): 
+#     result = 0
+#     if(a>b):
+#         result = get() + b
+#         result = 3 + b
+#         # return("Positive", result)
+    
+#     elif(a==b):
+#         # c= get()
+#         return("Something went wrong")
+        
+#     else:
+#         print("Nothing went wrong") 
+
+
+def Prints(x,a):
+    
+    if x > a:
+
+        y = x*3
+
+    return x    
+      
