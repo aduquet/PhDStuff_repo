b9b0162ec0c64f5d5fca9d45841ee7361e534b35
aduquet <duquet@ut.ee>
2022-08-18 15:17:02 +0200

    adding method FindElementsbyXpath

--- Metamorphic_testing/MRsfromMETWiki/webScrapping.py
+++ Metamorphic_testing/MRsfromMETWiki/webScrapping.py
@@ -18,35 +18,52 @@ def GetLinksbyLinkText(link_name):
 
     return driver.find_elements(By.LINK_TEXT, link_name).get_attribute('href')
 
-def FindElementensbyXpath(xpath):
+def FindElementenbyXpath(xpath):
 
     return driver.find_element('xpath', xpath)
 
+def FindElementsbyXpath(xpath):
+
+    return driver.find_elements('xpath', xpath)
+
 driver = InitWebDriver()
 
-driver.get('http://www.metwiki.net/')
+# driver.get('http://www.metwiki.net/')
 
-categories = ['number', 'machine', 'algorithm', 'geometry', 'opt', 'calculus', 'bio', 'graph']
+# categories = ['number', 'machine', 'algorithm', 'geometry', 'opt', 'calculus', 'bio', 'graph']
 
-mainDic = dict()
+# mainDic = dict()
 
-for cat in categories:
+# for cat in categories:
 
-    xpath_CategoryName = '//div[@class="part"]/div[@class="' + cat + '"]/div[@class="zi"]'
-    xpath_CategoryLink = '//div[@class="part"]/div[@class="' + cat + '"]/a'
+#     xpath_CategoryName = '//div[@class="part"]/div[@class="' + cat + '"]/div[@class="zi"]'
+#     xpath_CategoryLink = '//div[@class="part"]/div[@class="' + cat + '"]/a'
     
-    categoryName_aux = FindElementensbyXpath(xpath_CategoryName)
-    categoryLink_aux = FindElementensbyXpath(xpath_CategoryLink)
+#     categoryName_aux = FindElementensbyXpath(xpath_CategoryName)
+#     categoryLink_aux = FindElementensbyXpath(xpath_CategoryLink)
 
-    categoryName = categoryName_aux.text
-    categoryLink = categoryLink_aux.get_attribute('href')
+#     categoryName = categoryName_aux.text
+#     categoryLink = categoryLink_aux.get_attribute('href')
 
-    auxDic = { cat: {'name': categoryName, 'link_'+ cat: categoryLink}}
-    mainDic.update(auxDic)
+#     auxDic = { cat: {'name': categoryName, 'link_'+ cat: categoryLink}}
+#     mainDic.update(auxDic)
 
-with open("sample.json", "w") as outfile:
-    json.dump(mainDic, outfile, indent = 4)
+# mainDic['number'].update({'program': {'program_name': 'asdf', 'key_words': [1,2,3,4,5], 'asdfa': 'dfadf'}})
+# # mainDic.update(mainDic2)
+# print(mainDic['number'].keys())
+# print(mainDic)
 
-json_object = json.dumps(mainDic, indent = 4) 
-print(json_object)
- 
+# # with open("sample.json", "w") as outfile:
+#     # json.dump(mainDic, outfile, indent = 4)
+
+# json_object = json.dumps(mainDic, indent = 4) 
+# print(json_object)
+
+driver.get('http://www.metwiki.net/viewDomainProgram?domainName=Numerical%20program')
+
+xpath = '//ul[@class="repo-list"]/li[@class="repo-list-item"]/h3'
+
+a = FindElementsbyXpath(xpath)
+
+for i in a:
+    print(i.text)
