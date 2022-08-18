0174459377fc8d73b437114ee6423ad5fa6273e2
aduquet <duquet@ut.ee>
2022-08-18 14:19:48 +0200

    adding methods InitWebDriver, GetLinkbyLinkText, and FindElementesbyXpath

--- Metamorphic_testing/MRsfromMETWiki/webScrapping.py
+++ Metamorphic_testing/MRsfromMETWiki/webScrapping.py
@@ -1,7 +1,7 @@
-# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from shutil import which
+import json
 
 
 def InitWebDriver():
@@ -14,52 +14,39 @@ def InitWebDriver():
     driver = webdriver.Chrome(options=op)
     return driver
 
+def GetLinksbyLinkText(link_name):
+
+    return driver.find_elements(By.LINK_TEXT, link_name).get_attribute('href')
+
+def FindElementensbyXpath(xpath):
+
+    return driver.find_element('xpath', xpath)
+
 driver = InitWebDriver()
 
+driver.get('http://www.metwiki.net/')
 
-driver.get('http://www.metwiki.net/viewDomainProgram?domainName=Numerical%20program')
-movies_names = driver.find_elements('xpath','//ul[@class="repo-list"]/li[@class="repo-list-item"]/h3[@class="repo-list-name"]')
-# movies_names = driver.find_elements('xpath','//ul[@class="a-text-left mojo-field-type-title"]/a[@class="a-link-normal"]')
+categories = ['number', 'machine', 'algorithm', 'geometry', 'opt', 'calculus', 'bio', 'graph']
 
-# print(movies_names)
-movie_name_list = []
-d = []
+mainDic = dict()
 
-for movie in range(len(movies_names)):
-    movie_name_list.append(movies_names[movie].text)
-print(movie_name_list)
+for cat in categories:
 
-for movie_name_element in movie_name_list:
-    d.append(driver.find_element(By.LINK_TEXT, movie_name_element).get_attribute('href'))
+    xpath_CategoryName = '//div[@class="part"]/div[@class="' + cat + '"]/div[@class="zi"]'
+    xpath_CategoryLink = '//div[@class="part"]/div[@class="' + cat + '"]/a'
+    
+    categoryName_aux = FindElementensbyXpath(xpath_CategoryName)
+    categoryLink_aux = FindElementensbyXpath(xpath_CategoryLink)
 
-continue_link = driver.find_element(By.LINK_TEXT, movie_name_list[0]).get_attribute('href')
-print(len(d), len(movie_name_list))
+    categoryName = categoryName_aux.text
+    categoryLink = categoryLink_aux.get_attribute('href')
 
+    auxDic = { cat: {'name': categoryName, 'link_'+ cat: categoryLink}}
+    mainDic.update(auxDic)
 
-# products=[] #List to store name of the product
-# prices=[] #List to store price of the product
-# ratings=[] #List to store rating of the product
-# driver.get('http://www.metwiki.net/')
+with open("sample.json", "w") as outfile:
+    json.dump(mainDic, outfile, indent = 4)
 
-# driver.execute_script() 
-
-# EX:- driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-# ## Above Code Will Scrool Till The Bottom of Web Page
-# page_height = driver.execute_script("return document.body.scrollHeight")
-# for value in range(0,page_height):
-#  driver.execute_script(f"window.scrollTo(0, {value});")
-## Above Code Will Smooth Scrolling Till The End
-
-# URL = 'http://www.metwiki.net/'
-# page = requests.get(URL)
-
-# soup = BeautifulSoup(page.content, 'html.parser')
-# print(page.text)
-
-# htmlelement= driver.find_element_by_tag_name('html')
-# htmlelement.send_keys(Keys.END)
-
-# programes = ['Numerical program', 'Machine learning', 'Algorithm', 'Web application', 'Optimization algorithms', 'Bioinformatics', 'Graph and Image']
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-# button = driver.find_element('name',programes[0]).click()
-
+json_object = json.dumps(mainDic, indent = 4) 
+print(json_object)
+ 
