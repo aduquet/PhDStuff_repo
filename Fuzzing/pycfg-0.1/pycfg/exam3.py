fe7f6db7d231f85dd78c68bd626bff245cf9e58d
aduquet <duquet@ut.ee>
2022-08-23 20:17:11 +0200

    files

--- Fuzzing/pycfg-0.1/pycfg/exam3.py
+++
@@ -1,5 +0,0 @@
-from py2cfg import CFGBuilder
-
-cfg = CFGBuilder().build_from_file('example', 'C:/Users/duquet/Desktop/pycfg-0.1/pycfg-0.1/pycfg/expample.py')
-print(cfg)
-cfg.build_visual('exampleCFG', 'pdf')
